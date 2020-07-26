54baa72439c551695ba363d8d24de7a473cc1f31
CodePadawan27 <jarnodev7@gmail.com>
2020-07-26 22:45:04 +0300

    Updates

---
+++ precache-manifest.93fb615986dc5718c79b4bf9cb23d6a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60f85f307f50e6979ef64c2611b1899a",
+    "url": "/eventsmanager-app/index.html"
+  },
+  {
+    "revision": "673cfc6d9a0226e176f9",
+    "url": "/eventsmanager-app/static/css/main.5117f7b3.chunk.css"
+  },
+  {
+    "revision": "2b0af6e427182cdf20d5",
+    "url": "/eventsmanager-app/static/js/2.bbe8c1be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/eventsmanager-app/static/js/2.bbe8c1be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "673cfc6d9a0226e176f9",
+    "url": "/eventsmanager-app/static/js/main.929475f3.chunk.js"
+  },
+  {
+    "revision": "0d767aab2118e4d83efe",
+    "url": "/eventsmanager-app/static/js/runtime-main.1ff7c45c.js"
+  },
+  {
+    "revision": "0e221dfeff89e57e81916ec14d3b157b",
+    "url": "/eventsmanager-app/static/media/bg.0e221dfe.jpg"
+  }
+]);
